f513a9b405759c062c736ece4d0fe3926d6e2179
Robert Lucian Chiriac <robert.lucian.chiriac@gmail.com>
2021-11-17 16:12:22 +0200

    Fix the iris-classifier exampel

--- examples/rest-python-iris-classifier/handler.py
+++ examples/rest-python-iris-classifier/handler.py
@@ -1,24 +1,25 @@
-import mlflow.sklearn
+import os
+import pickle
 
+labels = ["setosa", "versicolor", "virginica"]
 
 class Handler:
     def __init__(self, config, model_client):
         self.client = model_client
 
     def load_model(self, model_path):
-        return mlflow.sklearn.load_model(model_path)
+        return pickle.load(open(os.path.join(model_path, "model.pkl"), "rb"))
 
     def handle_post(self, payload, query_params):
         model_version = query_params.get("version", "latest")
 
         model = self.client.get_model(model_version=model_version)
         model_input = [
-            payload["cylinders"],
-            payload["displacement"],
-            payload["horsepower"],
-            payload["weight"],
-            payload["acceleration"],
+            payload["sepal_length"],
+            payload["sepal_width"],
+            payload["petal_length"],
+            payload["petal_width"],
         ]
-        result = model.predict([model_input]).item()
+        label_index = model.predict([model_input]).item()
 
-        return {"prediction": result, "model": {"version": model_version}}
+        return {"prediction": labels[label_index], "model": {"version": model_version}}
